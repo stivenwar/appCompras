3c2e665759140422021a1d4a3cfbf2cfabcb4b68
Esteban <92550679+StivenWarSecond@users.noreply.github.com>
2022-07-26 17:05:50 +0200

    APP COMPRAS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ProveedoresService} from "./servicios/proveedores.service";
+import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HeaderComponent } from './header/header.component';
 
+const routes: RouterModule = [
+  { path: '', component: InicioComponent },
+  { path: 'proveedores', component: ProveedoresComponent },
+  { path: '**', component: InicioComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProveedoresComponent,
+    InicioComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(<Routes>routes)
   ],
-  providers: [],
+  providers: [ProveedoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
